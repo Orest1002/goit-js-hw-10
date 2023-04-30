dc9f5e342bfc5a53b299b2f784abcce6f4730c60
Orest <Orest347@gmail.com>
2023-04-30 18:20:47 +0300

    commit4

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import {fetchCountries} from './fetchCountries'
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
-
-
 const DEBOUNCE_DELAY = 300;
-
-
 
 const search = document.querySelector('#search-box')
 const countryList = document.querySelector('.country-list');
@@ -17,9 +13,7 @@ search.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(evt){
     evt.preventDefault()
-    
-   
-      
+     
     const inputValue = evt.target.value.trim();
     if (!inputValue) {
         resetMarkup(countryList);
@@ -27,16 +21,13 @@ function onSearch(evt){
         return
     }
 
-
-
-
     fetchCountries(inputValue)
     .then(dataCountry => {
       if (dataCountry.length > 10) {
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
-      } else if (dataCountry.length >= 2 || dataCountry.length <= 10) {
+      } if (dataCountry.length >= 2 && dataCountry.length <= 10) {
         resetMarkup(countryList);
         createMarkupList(dataCountry);
         resetMarkup(countryInfo);
@@ -52,17 +43,13 @@ function onSearch(evt){
       Notiflix.Notify.failure('Oops, there is no country with that name');
     });
 
-
-
 }
 function createMarkupList(dataCountry) {
     const markup = dataCountry
-      .map(({ name, capital, population, flags, languages }) => { 
-        return `<li><img src="${flags.svg}" alt="${name}" >
+      .map(({ name, flags,}) => { 
+        return `<li>
+         <img src="${flags.svg}" alt="${name}">
         <h2>${name.official}</h2>
-        <h3>${capital}</h3>
-        <p>${population}</p>
-        <p>${Object.values(languages).join(',')}</p>
      </li>`;
       })
       .join('');
@@ -70,13 +57,20 @@ function createMarkupList(dataCountry) {
   }
 
   function resetMarkup(el) {
-    el.innerHTML = '';
+    el.innerHTML = ('');
  }
 function createMarkupInfo(dataCountry){
-    return dataCountry.map(({ name, flags, })=> `
-    <li><img src="${flags}" alt="${name}">
-    <h2>${name}</h2>
- </li>`).join('');
- return countryList.insertAdjacentHTML('beforeend',markup);
+   markup = dataCountry.map(({ name, capital, population, flags, languages })=> {
+    return `
+    <li><img src="${flags.svg}" alt="${name}" >
+    <h2>${name.official}</h2>
+    <h3>${capital}</h3>
+    <p>${population}</p>
+    <p>${Object.values(languages).join('')}</p>
+  </li>`}).join('');
+  
+ return countryInfo.insertAdjacentHTML('beforeend',markup);
 }
 
+
+    
