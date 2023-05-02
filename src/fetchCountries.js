9221015e75be64e3ed88f5e35f6a3da99ddf5d64
Orest <Orest347@gmail.com>
2023-05-02 14:12:15 +0300

    commit17

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,22 @@
-const BASE_URL= ' https://restcountries.com/v3.1/name';
+// const BASE_URL= ' https://restcountries.com/v3.1/name';
 
-export function fetchCountries (name) {
+// export function fetchCountries (name) {
     
-      return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
-     .then(resp => {
-        if(!resp.ok){
-            throw new Error(resp.status)
-        }
-        return resp.json()
-     })
-};
+//       return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+//      .then(resp => {
+//         if(!resp.ok){
+//             throw new Error(resp.status)
+//         }
+//         return resp.json()
+//      })
+// };
+function fetchCountries(name) {
+    const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+    return fetch(URL).then(resp => {
+      if (!resp.ok) {
+        throw new Error(Notiflix.Notify.failure(`Oops, there is no country with that name.`));
+      }
+      return resp.json();
+    });
+  }
+  export { fetchCountries };
