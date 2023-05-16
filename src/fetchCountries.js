b9e4537a3c77e7b9ce174f93cf8e875ff0c7712a
Orest <Orest347@gmail.com>
2023-05-16 10:45:49 +0300

    task20

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,15 @@
-// const BASE_URL= ' https://restcountries.com/v3.1/name';
+const BASE_URL= ' https://restcountries.com/v3.1/name';
 
-// export function fetchCountries (name) {
+export function fetchCountries (name) {
     
-//       return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
-//      .then(resp => {
-//         if(!resp.ok){
-//             throw new Error(resp.status)
-//         }
-//         return resp.json()
-//      })
-// };
+      return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+     .then(resp => {
+        if(!resp.ok){
+            throw new Error(resp.status)
+        }
+        return resp.json()
+     })
+};
 // function fetchCountries(name) {
 //     const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 //     return fetch(URL).then(resp => {
@@ -20,19 +20,34 @@
 //     });
 //   }
 //   export { fetchCountries };
+// _____________________________
 
-  function fetchCountries(name) {
-    const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-    return fetch(URL)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Oops, there is no country with that name.');
-      })
-      .catch(error => {
-        Notiflix.Notify.failure(error.message);
-        return Promise.reject(error);
-      });
-  }
-  export { fetchCountries };
+  // function fetchCountries(name) {
+  //   const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+  //   return fetch(URL)
+  //     .then(response => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       }
+  //       throw new Error('Oops, there is no country with that name.');
+  //     })
+  //     .catch(error => {
+  //       Notiflix.Notify.failure(error.message);
+  //       return Promise.reject(error);
+  //     });
+  // }
+  // export { fetchCountries };
+
+
+  // ________________________________________________
+
+  // export function fetchCountries(searchQuery) {
+  //   return fetch(`https://restcountries.com/v3.1/name/${searchQuery}?fullText=true`)
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(response.status);
+  //       }
+  //       return response.json();
+  //     });
+  // }
+  // export { fetchCountries };
